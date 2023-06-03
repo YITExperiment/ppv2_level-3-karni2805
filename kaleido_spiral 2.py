c1464b45d097800e6ab9e5a20d28f34610553264
karni2805 <134511929+karni2805@users.noreply.github.com>
2023-06-03 10:30:01 +0530

    Add files via upload

--- kaleido_spiral 2.py
+++ kaleido_spiral 2.py
@@ -10,9 +10,9 @@ def draw_circle(size,angle,shift):
     turtle.circle(size)
     turtle.right(angle)
     turtle.forward(shift)
-    draw_circle(size, angle+10,shift+1)
+    draw_circle(size+10, angle+10,shift+1)
     
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(18)
+turtle.pensize(4)
 draw_circle(30,0,1)

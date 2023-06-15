6c2300eda23c2a73f1cf54ee0978852bca088e21
karni2805 <134511929+karni2805@users.noreply.github.com>
2023-06-15 20:01:50 +0530

    Add files via upload

---
+++ kalrido spiral 3.py
@@ -0,0 +1,18 @@
+import turtle
+
+from itertools import cycle
+colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple','pink','white'])
+
+
+def draw_circle(size,angle,shift):
+    turtle.bgcolor('black')
+    turtle.pencolor(next(colors))
+    turtle.circle(size)
+    turtle.right(angle)
+    turtle.forward(shift)
+    draw_circle(size+10, angle+10,shift+1)
+    
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(40)
+draw_circle(30,0,1)
